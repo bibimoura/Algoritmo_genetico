347a82e7a763a2793efd2aa3ba746c1b4d5470a1
Anna Beatriz Moura de Oliveira <mouraana53@gmail.com>
2025-06-25 02:24:13 -0300

    Mudança na elitismo.h

--- Elitismo/elitismo.h
+++ Elitismo/elitismo.h
@@ -13,14 +13,5 @@
 TSDoubleList* elitismo(TSDoubleList* , int , int );
 
 
-/**
- * Cria uma nova lista apenas com os que restaram da antiga;
- * @param TSDoubleList* lista Ponteiro;
- * @param int Taxa de elitismo;
- * @param int Tamanho da população;
- * @return TSDoubleList* ponteiro para a lista alocada.
- */
-TSDoubleList* elitismo_restante(TSDoubleList*, int , int );
-
 
 #endif
